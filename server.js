0a5e299ba8c8b10885950d2f57f831e877e83760
Skylldra <kily1010mc@gmail.com>
2024-11-28 23:01:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,22 +8,27 @@ const io = new Server(server);
 
 let players = [];
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); // Statische Dateien bereitstellen
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html"); // Root-Route
 });
 
 io.on("connection", (socket) => {
     console.log("Ein Nutzer hat sich verbunden");
 
+    // Spieler hinzufügen
     socket.on("add-player", (name) => {
-        if (!players.includes(name)) { // Spieler darf nur einmal hinzugefügt werden
+        if (!players.includes(name)) { // Spieler darf nicht doppelt hinzugefügt werden
             players.push(name);
-            io.emit("update-players", players); // Aktualisiere die Liste für alle
+            console.log("Spieler hinzugefügt:", name);
+            io.emit("update-players", players); // Aktualisiere die Liste für alle Clients
+        } else {
+            console.log("Spielername bereits vorhanden:", name);
         }
     });
 
+    // Verbindung trennen
     socket.on("disconnect", () => {
         console.log("Ein Nutzer hat die Verbindung getrennt");
     });
