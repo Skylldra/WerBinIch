90816403ded32df18f4258dcb72314135577f773
Skylldra <kily1010mc@gmail.com>
2024-11-29 15:58:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ let submittedWords = {};
 // Statische Dateien bereitstellen
 app.use(express.static(path.join(__dirname)));
 
-// Standard-Route für die Index-Seite
+// Route für die Indexseite
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
   socket.on('addPlayer', (name) => {
     if (!players.includes(name)) {
       players.push(name);
-      io.emit('updatePlayerList', players); // Aktualisiere die Liste für alle Clients
+      io.emit('updatePlayerList', players);
     }
   });
 
@@ -35,31 +35,33 @@ io.on('connection', (socket) => {
   socket.on('startGame', () => {
     if (players.length < 2) return;
 
-    // Zufällige Zuweisung von Spielern
     assignments = {};
     const shuffled = [...players].sort(() => Math.random() - 0.5);
     players.forEach((player, index) => {
       assignments[player] = shuffled[(index + 1) % players.length];
     });
 
-    // Wörter zurücksetzen
     submittedWords = {};
-
-    // Spielstatus an alle senden
     io.emit('gameStarted', { assignments, currentPlayer: socket.id });
   });
 
-  // Wort bestätigen
+  // Wort einreichen
   socket.on('submitWord', ({ player, word }) => {
     submittedWords[player] = word;
 
-    // Überprüfen, ob alle Spieler ihre Wörter eingereicht haben
     if (Object.keys(submittedWords).length === players.length) {
-      io.emit('allWordsSubmitted', submittedWords); // Alle Wörter an alle Clients senden
+      io.emit('allWordsSubmitted', submittedWords);
     }
   });
 
-  // Verbindung trennen
+  // Spiel zurücksetzen
+  socket.on('resetGame', () => {
+    players = [];
+    assignments = {};
+    submittedWords = {};
+    io.emit('updatePlayerList', players);
+  });
+
   socket.on('disconnect', () => {
     console.log('Ein Spieler hat die Verbindung getrennt:', socket.id);
   });
